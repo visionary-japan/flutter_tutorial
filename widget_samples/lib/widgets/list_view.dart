6e8b80d50eb1845976199e07ca00f37c356c2f66
Kurogoma939 <kan.sss.939@gmail.com>
2023-09-02 20:56:39 +0900

    ListViewのサンプル更新

--- widget_samples/lib/widgets/list_view.dart
+++ widget_samples/lib/widgets/list_view.dart
@@ -9,63 +9,104 @@ class ListViewSample extends StatelessWidget {
     return BaseScaffold(
       title: 'ListView Sample',
       child: SizedBox(
-        height: 150,
-        child: ListView(
-          scrollDirection: Axis.horizontal,
-          children: <Widget>[
-            Container(
-              height: 150,
-              width: 150,
-              margin: const EdgeInsets.symmetric(
-                vertical: 10,
-                horizontal: 10,
-              ),
-              color: Colors.red,
-              child: const Center(child: Text('a')),
+          child: ListView.builder(
+        itemCount: userList.length,
+        itemBuilder: (BuildContext context, int index) {
+          // ユーザーの単体データ
+          final User user = userList[index];
+          return Container(
+            height: 80,
+            color: Colors.blue,
+            margin: const EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 10,
             ),
-            Container(
-              height: 150,
-              width: 150,
-              margin: const EdgeInsets.symmetric(
-                vertical: 10,
-                horizontal: 10,
-              ),
-              color: Colors.red,
-              child: const Center(child: Text('a')),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(user.name),
+                const SizedBox(width: 10),
+                Text(user.age.toString()),
+              ],
             ),
-            Container(
-              height: 150,
-              width: 150,
-              margin: const EdgeInsets.symmetric(
-                vertical: 10,
-                horizontal: 10,
-              ),
-              color: Colors.red,
-              child: const Center(child: Text('a')),
-            ),
-            Container(
-              height: 150,
-              width: 150,
-              margin: const EdgeInsets.symmetric(
-                vertical: 10,
-                horizontal: 10,
-              ),
-              color: Colors.red,
-              child: const Center(child: Text('a')),
-            ),
-            Container(
-              height: 150,
-              width: 150,
-              margin: const EdgeInsets.symmetric(
-                vertical: 10,
-                horizontal: 10,
-              ),
-              color: Colors.red,
-              child: const Center(child: Text('a')),
-            ),
-          ],
-        ),
-      ),
+          );
+        },
+      )
+          // child: ListView(
+          //   scrollDirection: Axis.horizontal,
+          //   children: <Widget>[
+          // Container(
+          //   height: 150,
+          //   width: 150,
+          //   margin: const EdgeInsets.symmetric(
+          //     vertical: 10,
+          //     horizontal: 10,
+          //   ),
+          //       color: Colors.red,
+          //       child: const Center(child: Text('a')),
+          //     ),
+          //     Container(
+          //       height: 150,
+          //       width: 150,
+          //       margin: const EdgeInsets.symmetric(
+          //         vertical: 10,
+          //         horizontal: 10,
+          //       ),
+          //       color: Colors.red,
+          //       child: const Center(child: Text('a')),
+          //     ),
+          //     Container(
+          //       height: 150,
+          //       width: 150,
+          //       margin: const EdgeInsets.symmetric(
+          //         vertical: 10,
+          //         horizontal: 10,
+          //       ),
+          //       color: Colors.red,
+          //       child: const Center(child: Text('a')),
+          //     ),
+          //     Container(
+          //       height: 150,
+          //       width: 150,
+          //       margin: const EdgeInsets.symmetric(
+          //         vertical: 10,
+          //         horizontal: 10,
+          //       ),
+          //       color: Colors.red,
+          //       child: const Center(child: Text('a')),
+          //     ),
+          //     Container(
+          //       height: 150,
+          //       width: 150,
+          //       margin: const EdgeInsets.symmetric(
+          //         vertical: 10,
+          //         horizontal: 10,
+          //       ),
+          //       color: Colors.red,
+          //       child: const Center(child: Text('a')),
+          //     ),
+          //   ],
+          // ),
+          ),
     );
   }
+}
+
+const List<User> userList = [
+  User(name: 'John', age: 12),
+  User(name: 'Jane', age: 13),
+  User(name: 'Jack', age: 14),
+  User(name: 'Jill', age: 15),
+  User(name: 'James', age: 16),
+];
+
+// オブジェクト（モデルクラス）
+class User {
+  const User({
+    required this.name,
+    required this.age,
+  });
+
+  final String name;
+  final int age;
 }
